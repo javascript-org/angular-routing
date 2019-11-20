c1ea5aa1151ab48bfab709ba2b79165b8780f674
Rabindra Patra <RabindraPatra@corp.fairisaac.com>
2019-11-20 20:30:23 +0530

    doing login and logout \ fake

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -8,11 +9,19 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router:Router) { }
+  constructor(private router:Router, private authService:AuthService) { }
 
   ngOnInit() {
   }
 
+  onLogIn()
+  {
+    this.authService.loggedIn();
+  }
+  onLogOut()
+  {
+    this.authService.loggedOut();
+  }
   goToServer(id:number)
   {
     // programmatic navigation using queryParams and fragment
